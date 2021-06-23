3c60050e3fa1d1e47d956e084d31210d2c3018d4
Ganning Xu <ganningxu@gmail.com>
2021-06-23 11:02:26 -0400

    emotional function

---
+++ emotionalgifs/index.js
@@ -0,0 +1,15 @@
+
+const multipart = require('parse-multipart');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body;
+    var parts = multipart.Parse(body, boundary);
+    var convertedResult = Buffer.from(parts[0].data).toString('base64');
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: convertedResult
+    };
+}
