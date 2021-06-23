4b0de7260a029074ab449804bad765d5204e313f
Ganning Xu <ganningxu@gmail.com>
2021-06-23 16:15:55 -0400

    return img emotions

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,5 +1,6 @@
 
 const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -7,9 +8,42 @@ module.exports = async function (context, req) {
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
     var parts = multipart.Parse(body, boundary);
-    var convertedResult = Buffer.from(parts[0].data).toString('base64');
+    
+    // console.log(parts[0].data)
+    // var result = await analyzeImage(parts[0].data);
+    var result = await analyzeImage(body);
+    console.log(result)
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+}
+
+
+async function analyzeImage(img) {
+    const subKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+    })
+
+    let urlToUse = uriBase + '?' + params.toString();
+    console.log(urlToUse)
+    let resp = await fetch(urlToUse, {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,
+      
+      	//ADD YOUR TWO HEADERS HERE
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subKey
+        }
+    })
+
+    let data = await resp.json();
+    return data;
+
 }
