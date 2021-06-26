7cc8b82b5d6ac24f4ff637534ef3566b3e99c20c
Ganning Xu <ganningxu@gmail.com>
2021-06-26 13:38:32 -0400

    pls work

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -6,17 +6,21 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.log(connectionString)
-    
+
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
     var parsedBody = multipart.Parse(body, boundary);
-
+    context.log(parsedBody)
     var filetype = parsedBody[0].type;
     if (filetype == "image/png") {
         ext = "png";
     } else if (filetype == "image/jpeg") {
-        ext = "jpg";
-    } else {
+        ext = "jpeg";
+    } else if (filetype == "image/jpg") {
+        ext = "jpg"
+    }
+    
+    else {
         username = "invalidimage"
         ext = "";
     }
