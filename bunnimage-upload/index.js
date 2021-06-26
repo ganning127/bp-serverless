7cd89ec2e35ec9ab2a32627e2c10aeb42b0cc61f
Ganning Xu <ganningxu@gmail.com>
2021-06-26 14:03:11 -0400

    pls work 2

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,44 +1,58 @@
 const multipart = require('parse-multipart');
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+// const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=ganningstorage;AccountKey=N5XOc6bSeeK4gYn94OAehXRmZdfTLA36pLkb74N5nU1yjCojRDlZmQSehyK3PYuAx2mRAfCoP6MMvAcUEmiTkA==;EndpointSuffix=core.windows.net"
 const { BlobServiceClient } = require("@azure/storage-blob");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    context.log(connectionString)
+    let responseMessage;
 
+    // get the data from the request
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
-    var parsedBody = multipart.Parse(body, boundary);
-    context.log(parsedBody)
-    var filetype = parsedBody[0].type;
+    var headers = req.headers;
+
+    if (body == null) {
+        context.log("Undefined body image");
+        responseMessage = "Sorry! No image attached."
+    }
+    else {
+        var parsedBody = multipart.Parse(body, boundary);
+        // get the file extension
+        var ext = determineExt(parsedBody[0].type);
+        var password = headers.codename;
+        // upload the file to blob
+        responseMessage = await uploadFile(parsedBody, ext, password);
+    }
+    
+    context.log(responseMessage);
+    context.res = {
+        body: responseMessage
+    };
+}
+
+async function uploadFile(parsedBody, ext, filename) {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerName = "images";
+    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
+    const blobName = `${filename}.${ext}`;    // Create the container
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+    return uploadBlobResponse
+
+}
+
+function determineExt(filetype) {
     if (filetype == "image/png") {
         ext = "png";
     } else if (filetype == "image/jpeg") {
         ext = "jpeg";
     } else if (filetype == "image/jpg") {
         ext = "jpg"
-    }
-    
-    else {
+    } else {
         username = "invalidimage"
         ext = "";
     }
-
-    var responseMessage = await uploadFile(parsedBody, ext);
-    context.log(responseMessage);
-    context.res = {
-        body: "File Saved"
-    };
-}
-
-async function uploadFile(parsedBody, ext) {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-    const containerName = "images";
-    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
-    const blobName = 'test.' + ext;    // Create the container
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
-    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
-    return uploadBlobResponse
-
+    return ext
 }
