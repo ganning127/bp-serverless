71ef48bf7655ac37afc63721ee6febf9430d39bc
Ganning Xu <ganningxu@gmail.com>
2021-06-27 21:57:42 -0400

    if else

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,6 +1,5 @@
 const multipart = require('parse-multipart');
-// const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-const connectionString = "DefaultEndpointsProtocol=https;AccountName=ganningstorage;AccountKey=N5XOc6bSeeK4gYn94OAehXRmZdfTLA36pLkb74N5nU1yjCojRDlZmQSehyK3PYuAx2mRAfCoP6MMvAcUEmiTkA==;EndpointSuffix=core.windows.net"
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 const { BlobServiceClient } = require("@azure/storage-blob");
 
 module.exports = async function (context, req) {
@@ -13,7 +12,9 @@ module.exports = async function (context, req) {
     var body = req.body;
     var headers = req.headers;
 
-    try {
+    if (body == null) {
+        responseMessage = "Sorry! No image attached."
+    } else {
         var parsedBody = multipart.Parse(body, boundary);
         // get the file extension
         var ext = determineExt(parsedBody[0].type);
@@ -21,13 +22,7 @@ module.exports = async function (context, req) {
         // upload the file to blob
         responseMessage = await uploadFile(parsedBody, ext, password);
     }
-    catch(err) {
-        context.log("Undefined body image");
-        responseMessage = "Sorry! No image attached."
-    }
         
-    
-    context.log(responseMessage);
     context.res = {
         body: responseMessage
     };
@@ -41,7 +36,6 @@ async function uploadFile(parsedBody, ext, filename) {
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
     return uploadBlobResponse
-
 }
 
 function determineExt(filetype) {
