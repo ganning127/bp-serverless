23b6b7c474878d33ec0626a751d11bacbf2df165
Ganning Xu <ganningxu@gmail.com>
2021-06-27 22:15:54 -0400

    PLEASE

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,6 +1,6 @@
 const multipart = require('parse-multipart');
-// const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-const connectionString = "DefaultEndpointsProtocol=https;AccountName=ganningstorage;AccountKey=Oe40unfX1izdwX1wVplaEFmF/KKgPMq0odks9J2D1gObHNu1IR1fKOOL5oNQe88XTSVHjzZVhMDfA2rrhT3sjQ==;EndpointSuffix=core.windows.net"
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+
 const { BlobServiceClient } = require("@azure/storage-blob");
 
 module.exports = async function (context, req) {
@@ -14,11 +14,9 @@ module.exports = async function (context, req) {
     var headers = req.headers;
     context.log(body)
 
-    if (body == null) {
+    if (body == null || typeof(req.file) === "undefined") {
         responseMessage = "Sorry! No image attached."
-    } else if (body.length < 1000) {
-        responseMessage = "Sorry! No image attached."
-    }
+    } 
     else {
         var parsedBody = multipart.Parse(body, boundary);
         // get the file extension
