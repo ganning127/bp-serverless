05bec944112f41ca76c1e02c7f5e26a0f4a86fab
Ganning Xu <ganningxu@gmail.com>
2021-06-29 15:23:25 -0400

    try numero uno

---
+++ deepsecrets/index.js
@@ -0,0 +1,15 @@
+const querystring = require('querystring');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const queryObject = querystring.parse(req.body);
+    context.log(queryObject.Body)
+    // const bodyText = queryObject.Body;
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: `${queryObject.Body}`
+
+    };
+}
