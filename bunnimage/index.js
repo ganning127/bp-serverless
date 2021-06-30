22cec1642119c92c96c95aeeab158fb04c0aad67
Ganning Xu <ganningxu@gmail.com>
2021-06-30 10:54:59 -0400

    fetch js

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -1,8 +1,36 @@
 function getImage(event) {
-    event.preventDefault()
+    event.preventDefault();
     let nameInput = document.getElementById("name");
+    let fileInput = document.getElementById("image");
+
     if (nameInput.value != '') {
-        $('#output').text("Thanks!")
+        try {
+
+        
+        const endpoint = "https://bunnimage-function.azurewebsites.net/api/bunnimage-upload?code=1p6wLhuQBHpe/0gKVFD8i4ogEd2iRewBtWHTovdAejSOIZH7Y0mT/Q==";
+
+        
+        const file = fileInput.files[0];
+        var bunniForm = document.getElementById("bunniForm") 
+        var payload = new FormData(bunniForm);
+        payload.append("file", file)
+
+        const options = {
+            method: "POST",
+            body: payload,
+            headers: {
+                'codename': nameInput.value,
+              },
+        }
+        const response = fetch(endpoint, options);
+
+        $('#output').text("Your image has been stored successfully!")
+
+        } catch (e) {
+            $('#output').text(e)
+        }
+
+
     }
     else {
         alert("No name error.")
